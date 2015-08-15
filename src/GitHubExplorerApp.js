ef4b3f5de62f21b620fd53e082a4a686810c79c1
Leandro Ostera <leandro@ostera.io>
2015-08-15 15:42:14 +0200

    Works!

---
+++ src/GitHubExplorerApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class GitHubExplorerApp extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+        <div> Yeppers </div>
+      );
+  }
+};
